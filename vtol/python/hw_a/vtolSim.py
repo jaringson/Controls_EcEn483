885166dad3e5105589fadd9fb3153947a3002234
jaron <jaringson@gmail.com>
2017-09-15 14:57:55 -0600

    Fixxing vtol bug

--- vtol/python/hw_a/vtolSim.py
+++ vtol/python/hw_a/vtolSim.py
@@ -2,7 +2,7 @@ import matplotlib.pyplot as plt
 import numpy as np
 import sys
 sys.path.append('..')  # add parent directory
-import ballbeamParam as V
+import vtolParam as V
 from signalGenerator import signalGenerator
 from vtolAnimation import vtolAnimation
 from plotData import plotData
